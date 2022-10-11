e5dcbcda214f74ee38ebbd23dec53ec803af1b6c
Radu Cancel <radu@dontpayfull.com>
2022-10-11 20:52:02 +0300

    create base project

---
+++ parse_large_file/parse_large_file/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for parse_large_file project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'parse_large_file.settings')
+
+application = get_asgi_application()
